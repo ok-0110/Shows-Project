5f0097f65d3d9a4a366ca14782c6ba0a52690f32
ok big leptop <gunrh0123@gmail.com>
2022-05-19 13:06:15 +0300

    Add My-resume button

--- Project/client/my-app/src/App.js
+++ Project/client/my-app/src/App.js
@@ -8,11 +8,27 @@ import "./css/mainPage.css";
 import "./css/styleForShow.css";
 import "./css/styleForSubs.css";
 import "./css/styleForUsers.css";
-import "./css/intro.css"
+import "./css/intro.css";
+import "./css/resume.css";
 
 export default function App() {
   return (
     <div className="font300">
+      <br />
+      <div className="div101">
+        <a
+          className="a101"
+          href="https://drive.google.com/file/d/1LlZtG7WIAC6ZVDdYYB3-25ZMX9Rqolki/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          My resume
+        </a>
+      </div>
       <Main />
     </div>
   );
